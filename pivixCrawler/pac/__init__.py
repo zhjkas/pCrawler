eab8721455ef90adc5dd6fa2f3e035c0d7a1de3a
vivy~ <84112340+zhjkas@users.noreply.github.com>
2022-06-09 10:52:54 +0800

    1.0.0
    第一个版本！

---
+++ pivixCrawler/pac/__init__.py
@@ -0,0 +1,21 @@
+from pac.picDLRun import DownloadRun
+
+if __name__ == '__main__':
+    # 图片保存位置
+    print("请输入保存图片位置：")
+    filepath = input()  # r'D:\1code\pivixCrawler\图片保存'
+    # edge浏览器驱动位置
+    print("edge浏览器驱动位置：")
+    executable_path = input()  # r'D:\1code\pivixCrawler\edgedriver\msedgedriver.exe'
+    # 用户名
+    print("用户名")
+    username = input()  # 'zhonghualinjiang@gmail.com'
+    # 密码
+    print("密码")
+    password = input()  # 'nSVbHsc-J-!BGu6'
+    # 下载那一天的排行榜
+    print("下载那一天的排行榜 eg:20220603")
+    data = input()  # 20220601
+
+    running = DownloadRun(filepath, executable_path, username, password)
+    running.download(data)
